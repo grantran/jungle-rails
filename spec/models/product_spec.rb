ccc3359f30a451af8b93d5c1479a39f3a55312f1
grantran <grant.tran@gmail.com>
2017-06-12 19:27:15 +0000

    dried it up a bit

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,48 +2,45 @@ require 'rails_helper'
 
 
 RSpec.describe Product, type: :model do
+
   describe 'Validations' do
+    before(:each) do
+      @category = Category.create(name: 'Apparel')
+    end
+
     it 'should have all parameters' do
-      cat1 = Category.create(name: 'Apparel')
       p = Product.create({name: 'abc', description: 'why', 
-        category_id: cat1.id, quantity: 1, 
+        category_id: @category.id, quantity: 1, 
       price: 1000}) 
       expect(p).to be_valid
     end
 
     it 'should not be valid if the name is nil' do
-      cat1 = Category.create(name: 'Apparel')
       p = Product.create(name: nil, description: 'why', 
-        category_id: cat1.id, quantity: 1, 
+        category_id: @category.id, quantity: 1, 
       price: 1000)
       expect(p.errors.full_messages).to include("Name can't be blank")
     end 
       
     it 'should not be valid if the price is nil' do
-      cat1 = Category.create(name: 'Apparel')
       p = Product.create(name: 'a', description: 'why', 
-        category_id: cat1.id, quantity: 1, 
+        category_id: @category.id, quantity: 1, 
       price: nil)
       expect(p.errors.full_messages).to include("Price can't be blank")
     end 
     
     it 'should not be valid if the quantity is nil' do
-      cat1 = Category.create(name: 'Apparel')
       p = Product.create(name: 'a', description: 'why', 
-        category_id: cat1.id, quantity: nil, 
+        category_id: @category.id, quantity: nil, 
       price: 1000)
       expect(p.errors.full_messages).to include("Quantity can't be blank")
     end 
 
     it 'should not be valid if the category is nil' do
-      cat1 = Category.create(name: 'Apparel')
       p = Product.create(name: 'a', description: 'why', 
         category_id: nil, quantity: 1, 
       price: 1000)
       expect(p.errors.full_messages).to include("Category can't be blank")
     end 
-    
-    
-
   end
 end
